33ce5b47070c6eb08fc551abe2702764bc213083
Apdet <deterding1.a@gmail.com>
2020-06-01 21:25:41 -0500

    broke homepage into smaller components, finished styling menu-item component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
-
-import HomePage from './homepage.component';
-
 import './App.scss';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className='App'>
-        <HomePage />
-      </div>
-    );
-  }
+import HomePage from './pages/homepage/homepage.component';
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
