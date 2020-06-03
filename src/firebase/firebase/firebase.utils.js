532afab38be23bd6afb63fd0159b62b8eba7af97
Apdet <deterding1.a@gmail.com>
2020-06-03 13:06:21 -0500

    intergrated firebase into application, created sign-in component with sign in with google

---
+++ src/firebase/firebase/firebase.utils.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+
+const config = {
+   apiKey: "AIzaSyAyNo-5ev38k1Zn3nbbTTyufSp8Rg6GHP4",
+   authDomain: "urban-stylz-clothing.firebaseapp.com",
+   databaseURL: "https://urban-stylz-clothing.firebaseio.com",
+   projectId: "urban-stylz-clothing",
+   storageBucket: "urban-stylz-clothing.appspot.com",
+   messagingSenderId: "417189473691",
+   appId: "1:417189473691:web:f68d2223fdb63be6b6453e",
+   measurementId: "G-MYY9DD6YY6"
+ };
+
+ firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
